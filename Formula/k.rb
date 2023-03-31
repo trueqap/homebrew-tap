69a985b2ac24cc423a01496690a1147d686f80bf
David Backeus <duztdruid@gmail.com>
2023-03-31 17:12:21 +0200

    Add k formula

---
+++ Formula/k.rb
@@ -0,0 +1,21 @@
+# Installs the k command line interface
+
+class K < Formula
+  desc "Tool for managing the Reclaim the Stack platform"
+  homepage "https://github.com/reclaim-the-stack/k"
+  head "https://github.com/reclaim-the-stack/k.git"
+
+  depends_on "boz/repo/kail"
+  depends_on "int128/kubelogin/kubelogin"
+  depends_on "kubernetes-cli"
+  depends_on "kubeseal"
+  depends_on "yq"
+
+  def install
+    bin.install "k"
+  end
+
+  test do
+    system "#{bin}/k"
+  end
+end
---
+++ README.md
@@ -0,0 +1,9 @@
+# Reclaim the Stack: Homebrew Tap
+
+Install `k` via:
+
+```
+brew install --HEAD reclaim-the-stack/tap/k
+```
+
+
